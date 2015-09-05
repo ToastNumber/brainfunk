79d87fac80cdc765ee0d70243d43b0ef238849b5
ToastNumber <kelseyguitar55@googlemail.com>
2015-09-05 02:03:21 +0100

    Tape class complete

--- src/brainfunk/Tape.java
+++ src/brainfunk/Tape.java
@@ -3,19 +3,62 @@ package brainfunk;
 public class Tape {
 	private int[] tape = new int[30_000];
 	private int pointer = 0;
-	
+
 	private void throwIndexException() throws IndexOutOfBoundsException {
 		throw new IndexOutOfBoundsException("Tape value at " + pointer + " is negative.");
 	}
-	
+
 	public void perform(Action action) {
 		if (action == Action.INCREMENT) {
 			++tape[pointer];
 		} else if (action == Action.DECREMENT) {
-			if (tape[pointer] < 1) throwIndexException();
-			else --tape[pointer];
-		} else if (action == Action.LEFT) --pointer;
-		else if (action == Action.RIGHT) ++pointer;
-		else throw new IllegalArgumentException("This argument cannot be performed.");
+			if (tape[pointer] < 1)
+				throwIndexException();
+			else
+				--tape[pointer];
+		} else if (action == Action.LEFT)
+			--pointer;
+		else if (action == Action.RIGHT)
+			++pointer;
+		else
+			throw new IllegalArgumentException(String.format("\"%s\" cannot be performed.", action.toString()));
+	}
+
+	public int getValue() {
+		return tape[pointer];
+	}
+
+	public void assign(int value) {
+		tape[pointer] = value;
+	}
+
+	public int index() {
+		return pointer;
+	}
+
+	public String toString() {
+		String svaret = "";
+
+		for (int i = 0; i < 2 + 4 * pointer; ++i) {
+			svaret += " ";
+		}
+
+		svaret += "*";
+
+		svaret += "\n|";
+		// Find the end of the content
+		int index = 0;
+		for (int i = 0; i < tape.length; ++i) {
+			if (tape[i] != 0)
+				index = i + 1;
+		}
+
+		index = (int) Math.max(index, 10);
+
+		for (int i = 0; i < index; ++i) {
+			svaret += String.format(" %d |", tape[i]);
+		}
+
+		return svaret;
 	}
 }
