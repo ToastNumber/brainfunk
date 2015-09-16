73ca63cf26f5a311cc56a3e879132fbcfc951ea0
ToastNumber <kelseyguitar55@googlemail.com>
2015-09-16 02:12:37 +0100

    Input can be left blank

--- run.bat
+++ run.bat
@@ -1,16 +1,26 @@
 @ECHO off
 
 :main
-	ECHO BrainF%$k Interpreter
+	ECHO ---BrainFunk---
 	ECHO.
+	IF NOT EXIST bin/brainfunk/BrainFunk.class GOTO fail
+	
 	SET filepath=""
 	SET inputpath=""
 	
 	SET /p filepath="Enter file path: "
 	SET /p inputpath="Enter path of input: "
 
-	java -cp bin brainfunk.BrainFunk false %filepath% %inputpath%
+	IF %inputpath%=="" java -cp bin brainfunk.BrainFunk false %filepath%
+	IF NOT %inputpath%=="" java -cp bin brainfunk.BrainFunk false %filepath% %inputpath%
 
 	SET /p dummy="Press ENTER to continue ..."
 	CLS
 	GOTO main
+	
+:fail
+	ECHO Error: Could not find or load main class brainfunk.BrainFunk
+	ECHO Hint: Try compiling the source files using `compile.bat`
+	ECHO.
+	PAUSE
+	EXIT
